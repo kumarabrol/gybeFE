aa38d5693da7e8f58bae0094e3b5ac02441eddb1
seeddhee <73972398+seeddhee@users.noreply.github.com>
2024-09-10 00:18:35 -0700

    Almost got the put request working

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen'; // Adjust the path as necessary
-import DetailScreen from './src/screens/DetailScreen'; // Adjust the path as necessary
 import TaskScreen from './src/screens/TaskScreen';
 import TaskDatailScreen from './src/screens/TaskDetailScreen';
 
@@ -13,7 +12,6 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Detail" component={DetailScreen} />
         <Stack.Screen name="Task" component={TaskScreen} />
         <Stack.Screen name="TaskDetail" component={TaskDatailScreen} />
       </Stack.Navigator>
